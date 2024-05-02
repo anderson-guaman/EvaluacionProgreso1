a30d5988aee6d73c06f3c493755fac0b39870bab
Anderson Guaman <anderson.guaman@udla.edu.ec>
2024-05-02 07:33:56 -0500

    modelo AGuaman

---
+++ EvaluacionProgreso1/Models/AGuaman.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvaluacionProgreso1.Models
+{
+    public class AGuaman
+    {
+        [Key]
+        private int Id;
+
+        private float? Calificacion;
+        [Required]
+        private string Nombre;
+
+        private bool Ecuatoriano;
+        private DateTime FechaNacimiento;
+        [Required, ForeignKey("CarreraId")]
+        private int? CarreraId;
+        private Carrera? Carrera;
+    }
+}
