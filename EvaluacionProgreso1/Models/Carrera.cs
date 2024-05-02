f10adb33d0ea8cedbb06afeb53419354b975100c
Anderson Guaman <anderson.guaman@udla.edu.ec>
2024-05-02 08:00:43 -0500

    actualizacion de data anotacion en el modelo carrera

--- EvaluacionProgreso1/Models/Carrera.cs
+++ EvaluacionProgreso1/Models/Carrera.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EvaluacionProgreso1.Models
 {
@@ -7,10 +8,12 @@ namespace EvaluacionProgreso1.Models
         [Key]
         public int Id { get; set; }
         [Required,StringLength(30)]
+        [DisplayName("Numero de semestre")]
         public string Nombre_Carrera { get; set; }
         [Required]
         public string Campus {  get; set; }
         [Required]
+        [Range(1, 9)]
         public int Numero_Semestre { get; set; }
     }
 }
